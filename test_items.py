df33627011d10acb770972bba13cb4d8995adb3b
zaytsev <zaytsev@slotlogic.eu>
2025-04-07 16:49:49 +0200

    rewrite func name

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 selector = '[class="btn btn-lg btn-primary btn-add-to-basket"]'
 
 
-def test_site_have_an_basket(browser):
+def test_the_page_have_a_basket_button(browser):
     browser.get(link)
     assert browser.find_element(By.CSS_SELECTOR, selector), f"Не найдена кнопка добавления в корзину.."
 
