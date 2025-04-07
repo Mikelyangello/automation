2e1cfdf767dc3d948f48bd4bc3388cdb7c5512e7
zaytsev <zaytsev@slotlogic.eu>
2025-04-07 17:03:15 +0200

    add a try block for import private data

--- conftest.py
+++ conftest.py
@@ -3,7 +3,10 @@ import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from globals import MyCreds
+try:
+    from globals import MyCreds
+except ModuleNotFoundError:
+    print("You need to create a file\nglobals.py\nwith private credentials")
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver import FirefoxProfile
